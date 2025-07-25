e6945b170fe1e6789b8fc928c1e399ed3244b7b2
Saiyara <saiyaraislam17@gmail.com>
2025-07-25 00:03:10 -0400

    Update xab

--- christian/src/for_participant_xab_results.py
+++ christian/src/for_participant_xab_results.py
@@ -4,28 +4,32 @@ from torchvision import transforms
 from Net import *
 import numpy as np
 #compute distances only for pairs given in the excel file
+import matplotlib.pyplot as plt
 
 def clean_file():
     df = pd.read_csv("complete_xab_results.csv")
+
     new_type_column = []
     for i in range(len(df["A"])):
-        cat_A = df["A"].iloc[i].split("_0_")[1]
-        cat_B = df["B"].iloc[i].split("_0_")[1]
-        print(cat_A, cat_B)
-        if cat_A != cat_B:
+        cat_A = df["A"].iloc[i]
+        cat_B = df["B"].iloc[i]
+
+        if ("cat_0" in cat_A and "cat_1" in cat_B) or ("cat_1" in cat_A and "cat_0" in cat_B):
             new_type_column.append("between")
-        else:
-            if cat_A == "0":
-                new_type_column.append("within_0")
-            else:
-                new_type_column.append("within_1")
+        elif "cat_0" in cat_A and "cat_0" in cat_B:
+            new_type_column.append("within_0")
+        elif "cat_1" in cat_A and "cat_1" in cat_B:
+            new_type_column.append("within_1")
+
+    df["type"] = new_type_column
 
     df.to_csv("complete_xab_results.csv", index=False)
+
 
 def get_distances(weights_path, is_sup):
     if is_sup:
         unsup_model = Net()
-        unsup_model.load_state_dict(torch.load("../net_weights/slow_lr_freq/sup_net_weights_lr=0.0001 0 99.pth"))
+        unsup_model.load_state_dict(torch.load("../net_weights/unsup/unsup_net_weights_ lr= 0.0001 0 49.pth"))
 
         sup_model = SupervisedNet(unsup_model)
         sup_model.load_state_dict(torch.load(weights_path))
@@ -33,39 +37,38 @@ def get_distances(weights_path, is_sup):
         unsup_model = Net()
         unsup_model.load_state_dict(torch.load(weights_path))
 
-    between_dists = []
-    within_dists = []
+    d = {"between" :[],
+     "within_0" :[],
+     "within_1" :[]
+     }
 
     df = pd.read_csv("complete_xab_results.csv")
 
     transform = transforms.Compose([
-        transforms.Resize((128, 128)),  # Resize images to 128x128
-        transforms.ToTensor()  # Converts image to tensor and scales to [0, 1]
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+
     ])
 
-    for i in range(df["A"]):
-        im_A_path = "../" + df["A"].iloc[i].strip("./")
-        im_B_path = "../" + df["B"].iloc[i].strip("./")
-        im_A = transform(Image.open(im_A_path))
-        im_B = transform(Image.open(im_B_path))
+    for i in range(len(df["A"])):
+        im_A_path = "../../GABORS_400/gabors_1/" + df["A"].iloc[i].strip("./")
+        im_B_path = "../../GABORS_400/gabors_1/" + df["B"].iloc[i].strip("./")
+        im_A = transform(Image.open(im_A_path).convert('RGB')).reshape(1, 3, 128, 128)
+        im_B = transform(Image.open(im_B_path).convert('RGB')).reshape(1, 3, 128, 128)
+        stacked = torch.concat([im_A, im_B], dim=0)
 
-        _ = model(im_A)
         if is_sup:
-            mat_A = model.encoder_output
+            _ = sup_model(stacked)
+            stacked_mat = sup_model.encoder_output
 
         else:
-            mat_A = model.encoded
+            _ = unsup_model(stacked)
+            stacked_mat = unsup_model.encoded
 
+        type = df["type"].iloc[i]
 
-        _ = model(im_B)
-        if is_sup:
-            mat_B = model.encoder_output
+        mat_A, mat_B = torch.unbind(stacked_mat, dim=0)
 
-        else:
-            mat_B = model.encoded
-
-
-        type = df["xab_type_freq"].iloc[i]
 
         mat_A_flattened = mat_A.view(mat_A.size(0), -1)
         mat_B_flattened = mat_B.view(mat_B.size(0), -1)
@@ -74,11 +77,49 @@ def get_distances(weights_path, is_sup):
         mat_B_flattened_normalized = torch.nn.functional.normalize(mat_B_flattened, p=2, dim=1)
 
 
+        d[type].append(torch.norm(mat_A_flattened_normalized - mat_B_flattened_normalized).item())
 
-        if type == "between":
-            between_dists.append(torch.norm(mat_A_flattened_normalized - mat_A_flattened_normalized).item())
+
+    return np.mean(np.array(d["between"])), np.mean(np.array(d["within_0"])), np.mean(np.array(d["within_1"]))
+
+def dists(type_of_sup_training):
+    d = {"between": [],
+         "within_0": [],
+         "within_1": []
+         }
+    for i in range(150):
+        if i < 50:
+            weights_path = f"../net_weights/unsup/unsup_net_weights_ lr= 0.0001 0 {i}.pth"
+            b, w0, w1 = get_distances(weights_path=weights_path, is_sup=False)
         else:
-            within_dists.append(torch.norm(mat_B_flattened_normalized - mat_B_flattened_normalized).item())
+            weights_path = f"../net_weights/{type_of_sup_training}/sup_net_weights_lr=0.0001 0 {i-50}.pth"
+            b, w0, w1 = get_distances(weights_path=weights_path, is_sup=True)
+
+        d["between"].append(b)
+        d["within_0"].append(w0)
+        d["within_1"].append(w1)
+
+    df = pd.DataFrame(d)
+
+    df.to_csv(f"xab_{type_of_sup_training}_dists.csv", index=False)
+
+def plot():
+    df = pd.read_csv("xab_slow_lr_freq_dists.csv")
+    x = []
+    for i in range(150):
+        x.append(i)
 
 
-    return np.mean(np.array(between_dists)), np.mean(np.array(within_dists))
+    plt.plot(x, df['within_1'], color="limegreen", label="within Cat1")
+    plt.plot(x, df['within_0'], color="green", label="within Cat0")
+    plt.plot(x, df['between'], color="blue", label="between")
+
+    plt.axvline(x=49, color='r', linestyle='--')
+    plt.xlabel("Epoch")
+    plt.ylabel('Distance')
+    plt.title("Gabor categorization distances across training batches")
+    plt.show()
+
+if __name__ == "__main__":
+    #dists(type_of_sup_training="fast_lr_freq")
+    plot()
